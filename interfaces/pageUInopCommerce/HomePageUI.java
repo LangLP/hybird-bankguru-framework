cf83d65b47c5443ed582bd1ba6ba0bc2801990a4
Lang Lp <langlp1109@gmail.com>
2021-08-26 00:49:00 +0700

    Done_Register_Login_nopCommerce_lv3

---
+++ interfaces/pageUInopCommerce/HomePageUI.java
@@ -0,0 +1,16 @@
+package pageUInopCommerce;
+
+public class HomePageUI {
+    //public :phạm vi rộng ngoài  package
+	// static :biến tĩnh- cho phép 1 class khác truy cập đến biến của class này
+	// mà k cần khởi tạo đối tượng của class này lên
+	// HomePageUI.HOMEPAGE_SLIDER
+	// final:ngăn cản việc gán lại giá trị cho biến này
+	// static final : hằng số (contains) - phải viết hoa và phân cách bởi dấu gạch lối _
+	public static final String HOME_PAGE_SLIDER="//div[@id='nivo-slider']";
+	public static final String REGISTER_LINK="//a[@class='ico-register']";
+	public static final String LOGIN_LINK="//a[@class='ico-login']";
+	
+	
+	
+}
---
+++ testcases/com/nopcommerce/login/Level_03_Register_Login_Page_Object.java
@@ -0,0 +1,118 @@
+package com.nopcommerce.login;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import pageObjectnopCommerce.HomePageObject;
+import pageObjectnopCommerce.LoginPageObject;
+import pageObjectnopCommerce.RegisterPageObject;
+
+public class Level_03_Register_Login_Page_Object {
+
+	WebDriver driver;
+
+	String emailAddress, password;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println(projectPath);
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		emailAddress=getRandomEmail();
+		password="123456";
+	}
+
+	@Test
+	public void Login_01_Register_To_System() {
+
+		// Step 1 : mở Url ra -> Home Page
+		driver.get("https://demo.nopcommerce.com/");
+		homePage = new HomePageObject(driver);
+
+		// Step 02 :Verify Home Page logo displayed
+		Assert.assertTrue(homePage.isHomePageSliderDisplayed());
+
+		// Step 2 :Click to Register link -> Register Page
+		homePage.clickToRegisterLink();
+		registerPage = new RegisterPageObject(driver);
+		// Step 3:Click to Gender male radio
+		registerPage.clickToGenderMaleRadioButton();
+
+		// Step 4:Click to Firstname textbox
+		registerPage.enterToFirstnameTextbox("John");
+
+		// Step 5:Input to Lastname textbox
+		registerPage.enterToLastnameTextbox("Wick");
+
+		// Step 6:Input to Email textbox
+		registerPage.enterToEmailTextbox(emailAddress);
+
+		// Step 7:Input to Password textbox
+		registerPage.enterToPassworkTextbox(password);
+
+		// Step 8:Input to Confirm Password textbox
+		registerPage.enterToConfirmPasswordTextbox(password);
+
+		// Step 9:Click to Register button
+		registerPage.clickToRegisterButton();
+
+		// Step 10 :Verify Succes message displayed
+		Assert.assertTrue(registerPage.isSuccesMessageDisplayed());
+
+		// Step 11 :Click to Logout link ->Home page
+		registerPage.clickToLogoutLink();
+		homePage = new HomePageObject(driver);
+
+		// Step 12 :Verify Home Page logo displayed
+		Assert.assertTrue(homePage.isHomePageSliderDisplayed());
+	}
+
+	@Test
+	public void Login_02_Login_To_System() {
+
+		// Step 1:Click to Loginlink ->Home page
+		homePage.clickToLoginLink();
+		loginPage = new LoginPageObject(driver);
+
+		// Step 2:Input to Email textbox
+		loginPage.enterToEmailTextbox(emailAddress);
+
+		// Step 3:Input to Password textbox
+		loginPage.enterToPassworkTextbox(password);
+
+		// Step 4:Click to Login button
+		loginPage.clickToLoginButton();
+		homePage = new HomePageObject(driver);
+
+		// Step 12 :Verify Home Page logo displayed
+		Assert.assertTrue(homePage.isHomePageSliderDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+
+	}
+
+	public String getRandomEmail() {
+		Random rand = new Random();
+		return "Johndeep" + rand.nextInt(99999) + "@gmail.com";
+	}
+
+	HomePageObject homePage;
+	LoginPageObject loginPage;
+	RegisterPageObject registerPage;
+
+}
